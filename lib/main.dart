0e02a08783368143ed45a23d61b102d2284418d3
Parvin-Gasimli <parvinzga@code.edu.az>
2023-11-19 02:09:35 +0400

    Container and child

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,35 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.amber,
-            title: const Text('Flutter Demo Home Page')),
-        body: Container(
-          alignment: Alignment.center,
-          // margin: const EdgeInsets.all(20),
-          color: const Color.fromARGB(255, 39, 119, 176),
-          child: Text("salam"),
-          constraints: const BoxConstraints(
-            maxHeight: 200,
-            maxWidth: 200,
-            minHeight: 100,
-            minWidth: 100,
+            backgroundColor: const Color.fromARGB(255, 1, 1, 1),
+            title: const Text('Test App')),
+        body: Center(
+          widthFactor: 3,
+          heightFactor: 3,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 24, 23, 20),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 194, 184, 184),
+                  blurRadius: 10,
+                  spreadRadius: 2,
+                  offset: Offset(5, 5),
+                ),
+              ],
+            ),
+            alignment: Alignment.center,
+            child: FlutterLogo(
+              size: 124,
+            ),
+            padding: const EdgeInsets.all(20.0),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amber,
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
           hoverColor: Colors.amberAccent,
           onPressed: () {
             debugPrint('FAB clicked');
